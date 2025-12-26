ffc3582e8edc05f41103dd34963134b01815986f
Inacio Santana <127760321+Inaciosant@users.noreply.github.com>
2025-12-26 10:04:11 -0300

    Implementa estrutura básica da API com rotas para produtos e conexão ao banco de dados

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"api-go/controller"
+	"api-go/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+	ProductUseCase := usecase.NewProductUseCase()
+
+	ProductController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	server.GET("/products",ProductController.GetProducts)
+
+	server.Run(":8000")
+
+}
---
+++ controller/product_controller.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"api-go/model"
+	"api-go/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+type productController struct{
+	productUseCase usecase.ProductUsecase
+
+}
+
+func NewProductController(usecase usecase.ProductUsecase) *productController {
+    return &productController{
+		productUseCase: usecase,
+	}
+}
+func(p *productController)  GetProducts(ctx *gin.Context){
+	products := []model.Product{
+		{
+			ID:		1,
+			Name:	"Teste inacio",
+			Price:	20,
+		},
+	}
+	ctx.JSON(http.StatusOK,products)
+}
